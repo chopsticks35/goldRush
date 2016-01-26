147aaf446a36b84b1d1915b8633db0872d7f4087
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-01-26 12:06:31 -0700

    thru part 2

---
+++ script.js
@@ -0,0 +1,38 @@
+angular.module('myApp', []);
+
+angular.module('myApp')
+    .controller('myController', ['$scope', function($scope){
+        
+			$scope.coordinates = [];
+            $scope.show = false;
+			$scope.doClick = function($event) {
+                
+//              this logs coordiantes only
+//				var coords = {'x': 0, 'y': 0};
+//				coords['x'] = $event.pageX;
+//				coords['y'] = $event.pageY;
+//              $scope.coordinates.push(coords);
+//				console.log($scope.coordinates);
+                
+//              .spot with client or pageX top left
+                $scope.show = true;
+                $scope.spot = {  
+                'top': $event.pageY + "px",
+				'left': $event.pageX + "px",
+			     }
+                $scope.coordinates.push($scope.spot);
+                
+                console.log($scope.coordinates);
+                console.log($event);
+                console.log($scope.spot);
+                
+                $scope.removeIt = function($index){
+                $scope.coordinates.splice($index, 1);
+                }
+            }
+            
+    
+}])
+
+
+      
